f02773b9eeddf67c5d2659c14ad15d70db7bb912
avavhhh <158130193+avavhhh@users.noreply.github.com>
2026-03-09 11:59:53 +0800

    Update training parameters in config.py

--- config.py
+++ config.py
@@ -4,20 +4,20 @@ class Config:
     # ==========================
     # 训练基础参数
     # ==========================
-    epoch = 200
-    lr = 1e-3
+    epoch = 180
+    lr = 1e-4
     grad_norm = 0.5
     batchsize = 8
     weight_decay = 1e-5
     momentum = 0.9  # 原本的 mt 参数
-    power = 0.9
+    power = 0.5
 
     # ==========================
     # 数据与路径参数
     # ==========================
     trainsize = 352
     train_path = './data/TrainDataset'
-    train_save_name = 'iESegNet_result'
+    train_save_name = 'iESegNet_result_opt'
 
     # 自动生成的保存路径
     save_path = f'checkpoints/{train_save_name}/'
@@ -38,5 +38,6 @@ class Config:
     # ==========================
     # 记录与保存频率
     # ==========================
-    print_freq = 10  # 每多少个step打印一次日志
-    save_freq = 10  # 每多少个epoch保存一次权重
+    print_freq = 20  # 每多少个step打印一次日志
+    save_freq = 30  # 每多少个epoch保存一次权重
+
